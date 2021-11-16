bce6c6e9bbb27bf045869c328a9eb97ac8041746
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-11-16 10:39:13 -0300

    Merged lists

--- src/Input/index.js
+++ src/Input/index.js
@@ -3,12 +3,24 @@ import { getFakeData1, getFakeData2 } from '../fakeRequest'
 
 const Input = ({ inputValue, onChangeInputValue }) => {
   const [inputValueInComponent, setInputValueInComponent] = useState('');
+  let users = [];
 
   const fetchData = async () => {
     const firstNameWithId = await getFakeData1()
     const lastNameWithId = await getFakeData2()
 
+    users = firstNameWithId.map(user => {
+      const userLastname = lastNameWithId.find(item => item.id === user.id);
+      if (userLastname) {
+        const {lastName} = userLastname;
+        return {...user, lastName}
+      }
+    })
+    .filter(item => item !== undefined)
+    .sort((a, b) => a.firstName.localeCompare(b.firstName));
+
     console.log({firstNameWithId, lastNameWithId})
+    console.log(users);
   }
 
   useEffect(() => {
